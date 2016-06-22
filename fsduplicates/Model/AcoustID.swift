efc8c3ec8e4bcb3fb55d8327dd5f58559a2e827d
Andy Ibanez <andy@andyibanez.com>
2016-06-22 19:56:51 -0400

    Starting project.

---
+++ fsduplicates/Model/AcoustID.swift
@@ -0,0 +1,14 @@
+//
+//  AcoustID.swift
+//  fsduplicates
+//
+//  Created by Andy Ibanez on 6/22/16.
+//  Copyright © 2016 Andy Ibanez. All rights reserved.
+//
+
+import Foundation
+
+/// This singleton represents the [AcoustID](https://acoustid.org) service. It fetches fingerprint data from this service.
+class AcoustID {
+    
+}
