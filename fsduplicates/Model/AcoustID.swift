daa2694d2c58958946c0ac25c2cffb24e5029a71
Andy Ibanez <andy@andyibanez.com>
2016-06-22 21:38:04 -0400

    Checking client_id file existence, initialising AcoustID with its value if it does.

--- fsduplicates/Model/AcoustID.swift
+++ fsduplicates/Model/AcoustID.swift
@@ -11,4 +11,28 @@ import Foundation
 /// This singleton represents the [AcoustID](https://acoustid.org) service. It fetches fingerprint data from this service.
 class AcoustID {
     
+    /// Client ID (Registered on AcoustID's website).
+    let clientID: String
+    
+    /// Shared instance of the singleton.
+    static let instance = AcoustID()
+    
+    /// Initializes an AcoustID object. The Client ID should be set in the client_id.plist inside the Meta directory.
+    private init() {
+        let bundle = Bundle.main()
+        
+        
+        let fileManager = FileManager.default()
+            
+        var isDir: ObjCBool = false
+        if let clientIDPlistPath = bundle.pathForResource("client_id", ofType: "plist") where fileManager.fileExists(atPath: clientIDPlistPath, isDirectory: &isDir) {
+            if let clientFile = NSDictionary(contentsOfFile: clientIDPlistPath), let clientID = clientFile["client_id"] as? String {
+                self.clientID = clientID
+            } else {
+                fatalError("Error client_id value from client_id.plist file.")
+            }
+        } else {
+            fatalError("No client_id.plist file found")
+        }
+    }
 }
--- fsduplicates/main.swift
+++ fsduplicates/main.swift
@@ -10,3 +10,4 @@ import Foundation
 
 print("Hello, World!")
 
+AcoustID.instance
