ae0d2bfd773d58f023b5e8a9967bc3328c52ac21
Andy Ibanez <andy@andyibanez.com>
2016-06-23 00:46:28 -0400

    Checks for the -f flag completed.

--- fsduplicates/Helpers.swift
+++ fsduplicates/Helpers.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+// MARK: Useful types.
+
+// A touple to return all the required information from a valid directory check.
+typealias DirectoryCheckStatus = (isDirectory: Bool, message: String?)
+
+// MARK: Useful functions.
+
 /// Executes a shell command.
 ///
 /// This function was graciously copied, pasted, and adapted from [StackOverflow](http://stackoverflow.com/a/26972043/648767)
@@ -40,8 +47,28 @@ func consoleOutput(_ message: String) {
 
 /// Outputs the usage information.
 func usage() {
-    print("usage: fpcalc [OPTIONS] DIR_TO_SEARCH DIR_TO_OUTPUT\n")
+    print("usage: fsduplicates [OPTIONS]\n")
     print("Options:\n")
+    print(" -f DIR_TO_SEARCH DIR_TO_OUTPUT      Find duplicates in DIR_TO_SEARCH recursively. Output to DIR_TO_OUTPUT")
     print(" -fpcalc-path FPCALC_PATH            fpcalc executable path. Default is /usr/local/bin/fpcalc")
     print(" -v                                  Verbose mode.")
+}
+
+// Checks that valid path exists and that it is a directory.
+func validDirectory(path: String, parameterName: String) -> DirectoryCheckStatus {
+    var isDir: ObjCBool = false
+    
+    let message: String?
+    
+    if FileManager.default().fileExists(atPath: path, isDirectory: &isDir) {
+        if !isDir {
+            message = "\(parameterName): not a directory."
+        } else {
+            message = nil
+        }
+    } else {
+        message = "Invalid argument for \(parameterName)"
+    }
+    
+    return(Bool(isDir), message)
 }
--- fsduplicates/main.swift
+++ fsduplicates/main.swift
@@ -21,6 +21,8 @@ if arguments.contains("-h") {
 
 // We are gonna check that fpalc does exist, or that it was passed as a valid path. If fpalc is not found, exit early.
 
+// MARK: fpcalc validity checks
+
 if let fpcalcIndex = arguments.index(of: "-fpcalc-path") {
     // Ensuring that a path to fpcalc was passed.
     let pathIndex = fpcalcIndex + 1
@@ -30,17 +32,56 @@ if let fpcalcIndex = arguments.index(of: "-fpcalc-path") {
         exit(1)
     }
     
-    // Ensuring that fpcalc can be called at that specific path.
     fpcalcPath = arguments[pathIndex]
-    
-    let fpcalcExecutableExists = FileManager.default().isExecutableFile(atPath: fpcalcPath)
-    if !fpcalcExecutableExists {
-        print("\(fpcalcPath) does not exist. Exiting...")
-        exit(1)
-    }
 } else {
     consoleOutput("-fpcalc-path has not been specified. Will try using the default path (/usr/local/bin/fpcalc)")
     fpcalcPath = "/usr/local/bin/fpcalc"
+}
+
+// Ensuring that fpcalc does exist at the specified path and can be called.
+let fpcalcExecutableExists = FileManager.default().isExecutableFile(atPath: fpcalcPath)
+if !fpcalcExecutableExists {
+    print("\(fpcalcPath) does not exist. Exiting...")
+    exit(1)
+}
+
+// MARK: -f flag logic (if present).
+
+if let fFlagIndex = arguments.index(of: "-f") {
+    // This flag requires to extra arguments.
+    let minimumExpectedSize = fFlagIndex + 2
+    
+    if minimumExpectedSize >= arguments.count {
+        print("Missing arguments for -f")
+        usage()
+        exit(1)
+    }
+    
+    // Checking that the directories are valid.
+    
+    let sourceDir = arguments[fFlagIndex + 1]
+    let outputDir = arguments[fFlagIndex + 2]
+    
+    let (validSource, sourceMessage) = validDirectory(path: sourceDir, parameterName: "DIR_TO_SEARCH")
+    let (validOutput, outputMessage) = validDirectory(path: outputDir, parameterName: "DIR_TO_OUTPUT")
+    
+    if !validSource {
+        if let msg = sourceMessage {
+            print("\(msg)")
+        } else {
+            print("Unknown error")
+        }
+        exit(1)
+    }
+    
+    if !validOutput {
+        if let msg = outputMessage {
+            print("\(msg)")
+        } else {
+            print("Unknown error")
+        }
+        exit(1)
+    }
 }
 
 //print(shell(launchPath: "/usr/local/bin/fpcalc", arguments: ["-a"]))
